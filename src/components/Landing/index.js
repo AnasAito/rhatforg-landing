ac47d4e3873a800fad7addb4161724a726f7c320
AnasAito <khdimaty@gmail.com>
2021-05-23 16:40:15 +0100

    change butt colors

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -44,7 +44,7 @@ export default function Landing() {
                   <div className="mt-5 sm:mt-8 sm:flex justify-center ">
                     <div className="rounded-md shadow">
                       <Link to="/gallery">
-                        <button className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                        <button className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 md:py-4 md:text-lg md:px-10">
                           Gallery
                         </button>
                       </Link>
@@ -52,7 +52,7 @@ export default function Landing() {
                     <div className="mt-3 sm:mt-0 sm:ml-3">
                       <button
                         onClick={executeScroll}
-                        className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+                        className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-purple-100 hover:bg-purple-200 md:py-4 md:text-lg md:px-10"
                       >
                         Contact us
                       </button>
