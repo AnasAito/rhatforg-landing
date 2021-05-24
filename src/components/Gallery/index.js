cf7925e830e05a2b304f77f949707c612da05453
AnasAito <khdimaty@gmail.com>
2021-05-24 16:29:03 +0100

    add contact side bar

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import View from "./view";
 let assets = require("./data/final_assets.json");
 let entries = require("./data/final_entries.json");
-console.log(assets["1CIAQi7LNlD8XZ1siOBMXq"]);
+
 export default function index() {
   return <View entries={entries} assets={assets} />;
 }
