48325a93b3f024c6fe493bf973083a4d74e82de3
AnasAito <khdimaty@gmail.com>
2021-05-26 21:09:34 +0100

    add track

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import "../styles/styles.css";
 import Gallery from "./Gallery";
 import Landing from "./Landing/index";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import ReactGA from "react-ga";
+// Initialize google analytics using the tracking
+ReactGA.initialize("G-CJDBYS8RY6");
+ReactGA.pageview(window.location.pathname + window.location.search);
 export default function App() {
   return (
     <Router>
